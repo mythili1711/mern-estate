147d8d9aa5d40128b6cea7d0336def10b673aa38
C.MYTHILI <148332849+mythili2126j0525@users.noreply.github.com>
2025-05-16 15:43:36 +0530

    real estate

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'> 
+         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
+         </h1>
+        <form >
+       <input type='text' placeholder='Search... 'className='bg-transparent'/>
+
+        </form>
+        </div>
+     </header>
+  )
+}
+ 
